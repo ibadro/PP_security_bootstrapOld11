beebaa51dd40277a7af22721741c70df16e6ffbc
ilnurb <ilnurb@mail.ru>
2022-10-27 08:58:29 +0500

    !

--- src/main/java/pp/security_bootstrap1/controller/UserController.java
+++ src/main/java/pp/security_bootstrap1/controller/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pp.security_bootstrap1.model.User;
+import pp.security_bootstrap1.repository.UserRepo;
 import pp.security_bootstrap1.service.UserService;
 import pp.security_bootstrap1.service.UserServiceImpl;
 
@@ -13,17 +14,14 @@ import java.security.Principal;
 @Controller
 @RequestMapping("/user")
 public class UserController {
-
-    private final UserServiceImpl userServiceImpl;
     private final UserService userService;
-    public UserController(UserServiceImpl userServiceImpl, UserService userService) {
-        this.userServiceImpl = userServiceImpl;
+
+    public UserController(UserService userService) {
         this.userService = userService;
     }
 
     @GetMapping()
     public String getUser(Principal principal, Model model) {
-        model.addAttribute("logUser", userServiceImpl.findByUsername(principal.getName()));
         model.addAttribute("authorizedUser",
                 userService.findByUsername(principal.getName()));
         model.addAttribute("users", userService.findAllUsers());

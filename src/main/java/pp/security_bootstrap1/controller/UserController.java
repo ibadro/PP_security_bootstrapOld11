8cec35ca874049fe4bca4e9d4d00f36e36ee03d0
ilnurb <ilnurb@mail.ru>
2022-10-27 08:47:40 +0500

    !

--- src/main/java/pp/security_bootstrap1/controller/UserController.java
+++ src/main/java/pp/security_bootstrap1/controller/UserController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pp.security_bootstrap1.model.User;
+import pp.security_bootstrap1.service.UserService;
 import pp.security_bootstrap1.service.UserServiceImpl;
 
 import java.security.Principal;
@@ -13,14 +15,19 @@ import java.security.Principal;
 public class UserController {
 
     private final UserServiceImpl userServiceImpl;
-
-    public UserController(UserServiceImpl userServiceImpl) {
+    private final UserService userService;
+    public UserController(UserServiceImpl userServiceImpl, UserService userService) {
         this.userServiceImpl = userServiceImpl;
+        this.userService = userService;
     }
 
     @GetMapping()
     public String getUser(Principal principal, Model model) {
         model.addAttribute("logUser", userServiceImpl.findByUsername(principal.getName()));
+        model.addAttribute("authorizedUser",
+                userService.findByUsername(principal.getName()));
+        model.addAttribute("users", userService.findAllUsers());
+        model.addAttribute("newUser", new User());
         return "user/user";
     }
 }
